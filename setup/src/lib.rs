a0910643e969bfc828ca503d576d6f3520d9115f
quininer <quininer@live.com>
2025-06-19 15:24:58 +0800

    Check env switch for setup

--- setup/src/lib.rs
+++ setup/src/lib.rs
@@ -70,6 +70,13 @@ build_slot! {
 
 #[inline(always)]
 pub unsafe fn setup() {
+    if std::env::var_os("SFTRACE_OUTPUT_FILE").is_none() {
+        // Not enabled, ignored
+        return;
+    }
+
+    ENABLE_ALLOCATOR_HOOK.store(true, std::sync::atomic::Ordering::Relaxed);
+    
     unsafe {
         sftrace_setup(
             sftrace_entry_slot,
@@ -79,41 +86,62 @@ pub unsafe fn setup() {
     }
 }
 
+static ENABLE_ALLOCATOR_HOOK: std::sync::atomic::AtomicBool =
+    std::sync::atomic::AtomicBool::new(false);
+
 pub struct SftraceAllocator<A: GlobalAlloc>(pub A);
 
 unsafe impl<A: GlobalAlloc> GlobalAlloc for SftraceAllocator<A> {
     #[inline]
     unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        let enable = ENABLE_ALLOCATOR_HOOK.load(std::sync::atomic::Ordering::Relaxed);
+        
         unsafe {
             let v = std::alloc::System.alloc(layout);
-            sftrace_alloc_event(1, layout.size(), layout.align(), v);
+            if enable {
+                sftrace_alloc_event(1, layout.size(), layout.align(), v);
+            }
             v
         }
     }
 
     #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+        let enable = ENABLE_ALLOCATOR_HOOK.load(std::sync::atomic::Ordering::Relaxed);
+
         unsafe {
-            sftrace_alloc_event(2, layout.size(), layout.align(), ptr);
+            if enable {
+                sftrace_alloc_event(2, layout.size(), layout.align(), ptr);
+            }
+
             std::alloc::System.dealloc(ptr, layout);           
         }
     }
 
     #[inline]
     unsafe fn alloc_zeroed(&self, layout: std::alloc::Layout) -> *mut u8 {
+        let enable = ENABLE_ALLOCATOR_HOOK.load(std::sync::atomic::Ordering::Relaxed);
+
         unsafe {
             let v = std::alloc::System.alloc_zeroed(layout);
-            sftrace_alloc_event(1, layout.size(), layout.align(), v);
+            if enable {
+                sftrace_alloc_event(1, layout.size(), layout.align(), v);
+            }
             v
         }
     }
 
     #[inline]
     unsafe fn realloc(&self, ptr: *mut u8, layout: std::alloc::Layout, new_size: usize) -> *mut u8 {
+        let enable = ENABLE_ALLOCATOR_HOOK.load(std::sync::atomic::Ordering::Relaxed);
         unsafe {
-            sftrace_alloc_event(4, layout.size(), layout.align(), ptr);
+            if enable {
+                sftrace_alloc_event(4, layout.size(), layout.align(), ptr);
+            }
             let v = std::alloc::System.realloc(ptr, layout, new_size);
-            sftrace_alloc_event(3, new_size, layout.align(), v);
+            if enable {
+                sftrace_alloc_event(3, new_size, layout.align(), v);
+            }
             v            
         }
     }
