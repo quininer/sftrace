9569b4b960f587acd9f1b09b2ca4a4c440ec1b99
quininer <quininer@live.com>
2025-06-26 15:52:09 +0800

    setup: search solib by command

--- setup/build.rs
+++ setup/build.rs
@@ -1,12 +1,42 @@
+use std::path::PathBuf;
+
+
 fn main() {
     let name = "SFTRACE_DYLIB_DIR";
     
     println!("cargo::rerun-if-env-changed={}", name);
 
-    if let Ok(dir) = std::env::var(name) {
+    let path = match std::env::var(name).ok() {
+        Some(path) => Some(PathBuf::from(path)),
+        None => search_sftracelib_dir()
+    };
+
+    if let Some(dir) = path {
         let link = std::env::var("CARGO_MANIFEST_LINKS").unwrap();
         
-        println!("cargo::rustc-link-search=native={}", dir);
+        println!("cargo::rustc-link-search=native={}", dir.display());
         println!("cargo::rustc-link-lib=dylib={}", link);
+    }
+}
+
+fn search_sftracelib_dir() -> Option<PathBuf> {
+    use std::process::{ Command, Stdio };
+
+    let result = Command::new("sftrace")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .arg("record")
+        .arg("--print-solib")
+        .output();
+
+    match result {
+        Ok(output) if output.status.success() => {
+            let out = String::from_utf8(output.stdout).ok()?;
+            let mut out = PathBuf::from(out);
+            out.pop();
+            Some(out)
+        },
+        _ => None,
     }
 }
--- src/tools/record.rs
+++ src/tools/record.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::ffi::OsString;
-use std::path::PathBuf;
+use std::path::{ Path, PathBuf };
 use std::process::Command;
 use argh::FromArgs;
 use anyhow::Context;
@@ -24,11 +24,35 @@ pub struct SubCommand {
 
     /// command
     #[argh(positional, greedy)]
-    cmd: Vec<OsString>
+    cmd: Vec<OsString>,
+
+    /// try print solib path and exit
+    #[argh(switch)]
+    print_solib: bool
 }
 
 fn sftracelib() -> String {
     format!("{}sftrace{}", std::env::consts::DLL_PREFIX, std::env::consts::DLL_SUFFIX)
+}
+
+fn search_sftracelib(datadir: &Path) -> anyhow::Result<PathBuf> {
+    let name = sftracelib();
+
+    let path = datadir.join(&name);
+    if path.is_file() {
+        return Ok(path);
+    }
+    
+    let exd = env::current_exe()?;
+    let exd = exd
+        .parent()
+        .context("current exe should have a parent dir")?;
+    let path = exd.join(&name);
+    if path.is_file() {
+        return Ok(path);
+    }
+
+    anyhow::bail!("not found `{}`", name)
 }
 
 impl SubCommand {
@@ -37,17 +61,19 @@ impl SubCommand {
 
         #[cfg(target_os = "linux")]
         const LIBRARY_PATH_NAME: &str = "LD_LIBRARY_PATH";
+
+        let cwd = env::current_dir()?;
+        let projdir = directories::ProjectDirs::from("", "", "sftrace")
+            .context("not found project dir")?;        
+
+        if self.print_solib {
+            let path = search_sftracelib(projdir.data_dir())?;
+            print!("{}", path.display());
+            return Ok(())
+        }
         
         let mut iter = self.cmd.iter();
         let exe = iter.next().context("need command")?;
-
-        let cwd = env::current_dir()?;
-        let exd = env::current_exe()?
-            .parent()
-            .context("current exe should have a parent dir")?
-            .to_owned();
-        let projdir = directories::ProjectDirs::from("", "", "sftrace")
-            .context("not found project dir")?;
 
         let output = self.output.clone()
             .unwrap_or_else(|| cwd.join("sf.log"));
@@ -62,12 +88,10 @@ impl SubCommand {
         }
 
         if env::var_os(LIBRARY_PATH_NAME).is_none() {
-            let solib = self.solib.clone()
-                .or_else(|| Some(projdir.data_dir().join(sftracelib())))
-                .filter(|path| path.is_file())
-                .or_else(|| Some(exd.join(sftracelib())))
-                .filter(|path| path.is_file())
-                .with_context(|| format!("not found `{}`", sftracelib()))?;
+            let solib = match self.solib.as_ref() {
+                Some(p) => p.clone(),
+                None => search_sftracelib(&projdir.data_dir())?
+            };
 
             let libdir = solib.parent().unwrap();
             cmd.env(LIBRARY_PATH_NAME, libdir);
